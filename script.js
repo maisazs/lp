b6c94bce515900d476e14292510011918a15749f
Mayza Ester <130400582+maisazs@users.noreply.github.com>
2024-03-04 13:46:46 -0300

    att

--- script.js
+++ script.js
@@ -2,28 +2,18 @@
 
 var menu = document.querySelector('#nav-mobile');
 var fechar = document.querySelector('#fechar-menu');
-var li = document.getElementsByClassName('options-nav');
-var li2 = document.querySelector('li#btn-nav');
+var ul = document.getElementById('ul-nav');
 
 function openMenu() {
     menu.style.width = '100%';
     fechar.style.display = 'block';
-
-    for (let i = 0 ; i < li.length ; i++) {
-        li[i].style.display = 'flex';
-    }
-    li2.style.display = 'flex';
+    ul.style.display = 'flex';
 }
 
 function closeMenu() {
     menu.style.width = '0%';
-
     fechar.style.display = 'none';
-
-    for (let i = 0 ; i < li.length ; i++) {
-        li[i].style.display = 'none';
-    }
-    li2.style.display = 'none';
+    ul.style.display = 'none';
 }
 
 //------------------------------------------------------
@@ -32,7 +22,7 @@ function closeMenu() {
 var quadros = document.querySelector('#use-cases-quadros');
 
 function carroussel1() {
-    quadros.style.marginLeft = '10px';
+    quadros.style.marginLeft = '20px';
 }
 
 function carroussel2() {
